c3c54a21daaa337c06a07db2172b4015c009eee6
wangcch <wangcch.cc@gmail.com>
2020-09-30 10:11:57 +0800

    test: node test with @testing-library

--- tests/demo.test.tsx
+++ tests/demo.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { render } from "@testing-library/react";
 
 import Demo from "../src";
 
 test("Demo component text", () => {
-  // expect(demo.text()).toEqual("demo");
+  expect(render(<Demo />).container.textContent).toEqual("demo");
 });
